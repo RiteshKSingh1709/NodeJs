1a7d896d5cfb3197f2f170831cfc7ea2ab0ffff9
rishu-18 <ritesh.kumar465@gmail.com>
2018-04-17 11:46:56 +0530

    Added http and https support also with config file

---
+++ nodeREST/config.js
@@ -0,0 +1,29 @@
+/*
+*	configuration for envrionment specific
+*
+*/
+
+var envrionments = {};
+
+//envrionment specific to statgis
+envrionments.staging = {
+	'httpPort' : 3000,
+	'httpsPort' : 3001,
+	'envName' : 'staging'
+}
+
+//envrionment specific to production
+envrionments.production = {
+	'httpPort' : 5000,
+	'httpsPort' : 5001,
+	'envName' : 'production'
+}
+
+//get the current envrionment using command line argument
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+//export the crrent environment from the above or default to staging
+var currentEnvironmentToExport  = typeof(envrionments[currentEnvironment]) == 'object' ? envrionments[currentEnvironment] : envrionments.staging;
+
+// 
+module.exports = currentEnvironmentToExport;
--- nodeREST/index.js
+++ nodeREST/index.js
@@ -7,9 +7,39 @@
 var http = require('http');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
+var https = require('https');
+var fs = require('fs');
+//import config file
+var config = require('./config');
+console.log(config);
 
-// create the Server
-var createServer = http.createServer(function(req,res){
+// create the HTTP Server
+var httpServer = http.createServer(function(req,res){
+ 	unifiedServer(req,res);
+});
+
+//listen the HTTP port 
+httpServer.listen(config.httpPort,function(){
+	console.log("The HTTP server is listening at port " + config.httpPort + " in " + config.envName + " mode" );
+});
+
+
+//https option needs to be passed for creating HTTPS server
+var httpsOption = {
+	'key' : fs.readFileSync('../https/key.pem'),
+	'cert' : fs.readFileSync('../https/cert.pem'),
+};
+var httpsServer = https.createServer(httpsOption,function(req,res){
+	unifiedServer(req,res);
+});
+
+//listen the HTTPS port 
+httpServer.listen(config.httpsPort,function(){
+	console.log("The HTTPS server is listening at port " + config.httpsPort + " in " + config.envName + " mode" );
+});
+
+//unified server logic same for HTTP and HTTPS
+var unifiedServer = function(req,res){
 
 	//get the url and parse
 	// url.parse take two param one the requested url and "true" is for parsing query string
@@ -65,31 +95,22 @@ var createServer = http.createServer(function(req,res){
 			console.log(payloadString);
 			
 			//returnt the response 
+			res.setHeader('Content-Type','application/json');
 			res.writeHead(statusCode);
 
 			res.end(payloadString);
 
 			//log the requested path with method name and query String (if any)
-			console.log("The request path : "+trimmedUrl+ " and the method name is : "+method+"with query string : ",queryStringObject);
+			console.log("The request path : "+trimmedUrl+ " and the method name is : "+method+" with query string : ",queryStringObject);
 			//console.log("The request header is :",header);
 			//console.log("the payload is :" , buffer);
 
 		});
 
-		
-		//send the response
-
 	});
 
 
-
-});
-
-//listen the port 
-
-createServer.listen(3000,function(){
-	console.log("The server is listening at port 3000");
-});
+};
 
 //Define the handler
 var handler = {};
